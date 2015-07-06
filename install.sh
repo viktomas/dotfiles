73305d4998d472a2c867e9849f2d7ce4bc0b0bd4
Tomas Vik <tomas.vik@rea-group.com>
2015-07-06 14:36:46 +1000

    Update install script

--- install.sh
+++ install.sh
@@ -1,12 +1,13 @@
 #!/bin/sh
 
-.git
-.gitconfig
-.githelpers
-.gitignore
-.gitmodules
-.i3
-.vim
-.vimrc
-.zshrc
-bin
+ln -s ./.bashrc ~/.bashrc
+ln -s ./.gitconfig ~/.gitconfig
+ln -s ./.githelpers ~/.githelpers
+ln -s ./.i3 ~/.i3
+ln -s ./.profile ~/.profile
+ln -s ./.vim ~/.nvim
+ln -s ./.vim ~/.vim
+ln -s ./.vimrc ~/.nvimrc
+ln -s ./.vimrc ~/.vimrc
+ln -s ./.zshrc ~/.zshrc
+ln -s ./bin ~/bin
