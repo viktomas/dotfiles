28a1a6630bd527299bdf815229dc4e65f83a27ef
Tomas Vik <tomas.vik@rea-group.com>
2016-03-04 22:13:39 +1100

    Retarded breaking change of nvim

--- uninstall.sh
+++ uninstall.sh
@@ -6,8 +6,11 @@ rm -rf ~/.githelpers
 rm -rf ~/.i3
 rm -rf ~/.profile
 rm -rf ~/.nvim
+rm -rf ~/.config/nvim
 rm -rf ~/.vim
 rm -rf ~/.nvimrc
 rm -rf ~/.vimrc
 rm -rf ~/.zshrc
+rm -rf ~/tmux.cheat
+rm -rf ~/.nvpy.cfg
 rm -rf ~/bin
