abad5b71e9dcab80ccef9417de045a479fe89ff5
Tomas Vik <tvik@gitlab.com>
2020-11-22 17:05:23 +0100

    Fixing Darwin typo

--- install.sh
+++ install.sh
@@ -6,7 +6,7 @@ ln -s $LOCATION/.profile ~/.profile
 ln -s $LOCATION/vim ~/.config/nvim
 ln -s $LOCATION/.zshrc ~/.zshrc
 [ "$(uname 2> /dev/null)" == "Linux" ] && ln -s $LOCATION/.zshrc-Linux ~/.zshrc-Linux
-[ "$(uname 2> /dev/null)" == "Dawin" ] && ln -s $LOCATION/.zshrc-Dawin ~/.zshrc-Dawin
+[ "$(uname 2> /dev/null)" == "Darwin" ] && ln -s $LOCATION/.zshrc-Darwin ~/.zshrc-Darwin
 ln -s $LOCATION/.ag-ignore ~/.ag-ignore
 ln -s $LOCATION/.credorc ~/.credorc
 ln -s $LOCATION/bin/ ~/
@@ -14,7 +14,7 @@ ln -s $LOCATION/.global-gitignore ~/.gitignore
 ln -s $LOCATION/.taskbook.json ~/.taskbook.json
 ln -s $LOCATION/.goduignore ~/.goduignore
 ln -s $LOCATION/.notable.json ~/.notable.json
-[ "$(uname 2> /dev/null)" == "Dawin" ] && ln -s "$LOCATION/vscode/settings.json" "$HOME/Library/Application Support/Code/User/settings.json"
+[ "$(uname 2> /dev/null)" == "Darwin" ] && ln -s "$LOCATION/vscode/settings.json" "$HOME/Library/Application Support/Code/User/settings.json"
 [ "$(uname 2> /dev/null)" == "Linux" ] && ln -s "$LOCATION/vscode/settings.json" "/home/tomas/.config/Code - OSS/User/settings.json"
 ln -s ~/Dropbox/.gitconfig ~/.gitconfig
 cd ~/.dotfiles
