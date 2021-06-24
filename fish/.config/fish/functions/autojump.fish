dd5e9ba709031493cd5f025c7904f2562d0e6cce
Tomas Vik <tvik@gitlab.com>
2021-06-24 10:31:54 +0200

    fix fish autojump initializatoin on mac

--- fish/.config/fish/functions/autojump.fish
+++ fish/.config/fish/functions/autojump.fish
@@ -1,5 +1,3 @@
-if test -e /usr/share/autojump/autojump.fish
-  source /usr/share/autojump/autojump.fish
-end
-[ -f /usr/local/share/autojump/autojump.fish ]; and source /usr/local/share/autojump/autojump.fish
+[ -e /usr/share/autojump/autojump.fish ]; and source /usr/share/autojump/autojump.fish
+[ -e /usr/local/share/autojump/autojump.fish ]; and source /usr/local/share/autojump/autojump.fish
 
