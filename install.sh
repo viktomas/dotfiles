8eacd2b12b6c431bbe0504c73d145829b26d1294
Tomas Vik <tvik@gitlab.com>
2021-12-06 15:28:11 +0100

    removing ranger and taskbook

--- install.sh
+++ install.sh
@@ -8,10 +8,8 @@ ln -s $LOCATION/.zshrc ~/.zshrc
 [ "$(uname 2> /dev/null)" == "Linux" ] && ln -s $LOCATION/.zshrc-Linux ~/.zshrc-Linux
 [ "$(uname 2> /dev/null)" == "Darwin" ] && ln -s $LOCATION/.zshrc-Darwin ~/.zshrc-Darwin
 ln -s $LOCATION/.ag-ignore ~/.ag-ignore
-ln -s $LOCATION/.credorc ~/.credorc
 ln -s $LOCATION/bin/ ~/
 ln -s $LOCATION/.global-gitignore ~/.gitignore
-ln -s $LOCATION/.taskbook.json ~/.taskbook.json
 ln -s $LOCATION/.goduignore ~/.goduignore
 ln -s $LOCATION/.notable.json ~/.notable.json
 [ "$(uname 2> /dev/null)" == "Darwin" ] && ln -s "$LOCATION/vscode/settings.json" "$HOME/Library/Application Support/Code/User/settings.json"
@@ -19,7 +17,6 @@ ln -s $LOCATION/.notable.json ~/.notable.json
 ln -s ~/Dropbox/.gitconfig ~/.gitconfig
 cd ~/.dotfiles
 stow kitty
-stow ranger
 stow fish
 stow fd
 
