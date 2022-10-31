0ba63edbadd6414cc385fa8cf58be0946de012af
Tomas Vik <me@viktomas.com>
2022-10-31 10:06:35 +0100

    different git config for work and private repos

--- install.sh
+++ install.sh
@@ -3,6 +3,9 @@ LOCATION="$( cd "$( dirname "$0" )" >/dev/null 2>&1 && pwd )"
 ln -s $LOCATION/.ignore ~/.ignore
 ln -s $LOCATION/.crontab ~/.crontab
 ln -s $LOCATION/.githelpers ~/.githelpers
+ln -s $LOCATION/.gitconfig ~/.gitconfig
+ln -s $LOCATION/.gitconfig-work ~/.gitconfig-work
+ln -s $LOCATION/.gitconfig-private ~/.gitconfig-private
 ln -s $LOCATION/.profile ~/.profile
 ln -s $LOCATION/vim ~/.config/nvim
 # Create files for vim backup/undo
@@ -15,7 +18,6 @@ ln -s $LOCATION/.notable.json ~/.notable.json
 [ "$(uname 2> /dev/null)" == "Darwin" ] && ln -s "$LOCATION/vscode/settings.json" "$HOME/Library/Application Support/Code/User/settings.json"
 [ "$(uname 2> /dev/null)" == "Darwin" ] && ln -s "$LOCATION/vscode/settings.json" "$HOME/Library/Application Support/Code - Insiders/User/settings.json"
 [ "$(uname 2> /dev/null)" == "Linux" ] && ln -s "$LOCATION/vscode/settings.json" "$HOME/.config/Code - OSS/User/settings.json"
-ln -s ~/Dropbox/.gitconfig ~/.gitconfig
 cd ~/.dotfiles
 stow kitty
 stow fish
