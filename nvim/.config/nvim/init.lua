daeadef7f8efb5beed752b6e22b56923e86395c5
Tomas Vik <me@viktomas.com>
2022-12-03 09:11:57 +0100

    move neovim plugins from plug to packer

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -1,46 +1,4 @@
-local Plug = vim.fn['plug#']
-
-vim.call('plug#begin', '~/.config/nvim/plugged')
-
--- color scheme
-Plug 'morhetz/gruvbox'
-Plug 'vim-airline/vim-airline'
-
--- git related plugins
-Plug 'tpope/vim-fugitive'
-Plug 'airblade/vim-gitgutter'
-
--- language related plugins
-Plug 'sheerun/vim-polyglot'
-Plug('nvim-treesitter/nvim-treesitter', {['do'] = ':TSUpdate'})
-Plug 'neovim/nvim-lspconfig'
-
-
--- file orientation plugins
-Plug 'tpope/vim-vinegar'
-Plug 'justinmk/vim-sneak'
-Plug 'tpope/vim-surround'
-Plug 'junegunn/fzf'
-Plug 'junegunn/fzf.vim'
-Plug 'milkypostman/vim-togglelist'
-
--- Autocomplete
-Plug 'hrsh7th/cmp-nvim-lsp'
-Plug 'hrsh7th/cmp-buffer'
-Plug 'hrsh7th/cmp-path'
-Plug 'hrsh7th/cmp-cmdline'
-Plug 'hrsh7th/nvim-cmp'
-Plug 'hrsh7th/vim-vsnip'
-
--- general code helping plugins
-Plug 'tpope/vim-commentary'
-Plug 'w0rp/ale'
-Plug ('prettier/vim-prettier', {
-  ['do'] = 'yarn install',
-  ['for'] = {'javascript', 'typescript', 'css', 'less', 'scss', 'json', 'graphql', 'vue', 'yaml', 'html'},
-})
-
-vim.call('plug#end')
+require('user.plugins')
 
 vim.cmd('source ' .. '~/.config/nvim/old-config.vim')
 
---
+++ nvim/.config/nvim/lua/user/plugins.lua
@@ -0,0 +1,60 @@
+local ensure_packer = function()
+  local fn = vim.fn
+  local install_path = fn.stdpath('data')..'/site/pack/packer/start/packer.nvim'
+  if fn.empty(fn.glob(install_path)) > 0 then
+    fn.system({'git', 'clone', '--depth', '1', 'https://github.com/wbthomason/packer.nvim', install_path})
+    vim.cmd [[packadd packer.nvim]]
+    return true
+  end
+  return false
+end
+
+local packer_bootstrap = ensure_packer()
+
+return require('packer').startup(function(use)
+  use 'wbthomason/packer.nvim'
+
+  -- color scheme
+  use 'morhetz/gruvbox'
+  use 'vim-airline/vim-airline'
+
+  -- git related plugins
+  use 'tpope/vim-fugitive'
+  use 'airblade/vim-gitgutter'
+
+  -- language related plugins
+  use 'sheerun/vim-polyglot'
+  use {'nvim-treesitter/nvim-treesitter', ['do'] = ':TSUpdate'}
+  use 'neovim/nvim-lspconfig'
+
+
+  -- file orientation plugins
+  use 'tpope/vim-vinegar'
+  use 'justinmk/vim-sneak'
+  use 'tpope/vim-surround'
+  use 'junegunn/fzf'
+  use 'junegunn/fzf.vim'
+  use 'milkypostman/vim-togglelist'
+
+  -- Autocomplete
+  use 'hrsh7th/cmp-nvim-lsp'
+  use 'hrsh7th/cmp-buffer'
+  use 'hrsh7th/cmp-path'
+  use 'hrsh7th/cmp-cmdline'
+  use 'hrsh7th/nvim-cmp'
+  use 'hrsh7th/vim-vsnip'
+
+  -- general code helping plugins
+  use 'tpope/vim-commentary'
+  use 'w0rp/ale'
+  use {'prettier/vim-prettier',
+    ['do'] = 'yarn install',
+    ['for'] = {'javascript', 'typescript', 'css', 'less', 'scss', 'json', 'graphql', 'vue', 'yaml', 'html'},
+  }
+
+  -- Automatically set up your configuration after cloning packer.nvim
+  -- Put this at the end after all plugins
+  if packer_bootstrap then
+    require('packer').sync()
+  end
+end)
