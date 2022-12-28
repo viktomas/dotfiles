50f5f5a2a64b2faaab73a3cadda4591569ac66c7
Tomas Vik <me@viktomas.com>
2022-12-28 07:36:33 +0100

    vim: disable treesitter folding till telescope fixes the issue

--- nvim/.config/nvim/lua/user/treesitter.lua
+++ nvim/.config/nvim/lua/user/treesitter.lua
@@ -23,18 +23,19 @@ require'nvim-treesitter.configs'.setup {
 -- This is partially broken thanks to telescope
 -- see issue https://github.com/nvim-telescope/telescope.nvim/issues/559
 -- and https://github.com/nvim-telescope/telescope.nvim/issues/699
-vim.opt.foldmethod = "expr"
-vim.opt.foldexpr = "nvim_treesitter#foldexpr()"
+-- UNCOMENT FOLLOWING LINES IF Telescope ever fixes the issue
+-- vim.opt.foldmethod = "expr"
+-- vim.opt.foldexpr = "nvim_treesitter#foldexpr()"
 
 -- workaraound for telescope issue https://github.com/nvim-telescope/telescope.nvim/issues/559#issuecomment-1074076011
-vim.api.nvim_create_autocmd('BufRead', {
-   callback = function()
-      vim.api.nvim_create_autocmd('BufWinEnter', {
-         once = true,
-         command = 'normal! zx zR'
-      })
-   end
-})
+-- vim.api.nvim_create_autocmd('BufRead', {
+--    callback = function()
+--       vim.api.nvim_create_autocmd('BufWinEnter', {
+--          once = true,
+--          command = 'normal! zx zR'
+--       })
+--    end
+-- })
 
 -- this is my rewrite of the following autocmd
 -- autocmd BufReadPost,FileReadPost * normal zR
