3730888e72e2bba91e494aeefe85eb5a69b86a30
Tomas Vik <me@viktomas.com>
2023-01-14 08:43:58 +0100

    different indenting for go

---
+++ nvim/.config/nvim/ftplugin/go.lua
@@ -0,0 +1,9 @@
+local options = {
+  tabstop = 4,                             -- insert 4 spaces for a tab
+  expandtab = false,                       -- convert tabs to spaces
+  shiftwidth = 4,                          -- the number of spaces inserted for each indentation
+}
+
+for k, v in pairs(options) do
+  vim.opt[k] = v
+end
