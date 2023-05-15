566cf8fe16a440ce9d34296f78e7edcdf902e8eb
Tomas Vik <me@viktomas.com>
2023-05-15 14:10:39 +0200

    fish: run config.fish only in interactive mode

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -1,37 +1,40 @@
 set fish_greeting # Turn off greeting
-fish_vi_key_bindings # vi insert mode
-
 source ~/.profile
 
-function fish_user_key_bindings
-    for mode in insert default visual
-        bind -M $mode \cf forward-char
-    end
-end
+if status is-interactive
+    fish_vi_key_bindings # vi insert mode
 
-# init zoxide
-complete -e j
-if command -sq zoxide
-    zoxide init --cmd j fish | source
-else
-    echo 'zoxide: command not found, please install it from https://github.com/ajeetdsouza/zoxide'
-end
 
-atuin init fish | source
-
-if type -q brew
-
-    [ -e (brew --prefix asdf)/libexec/asdf.fish ]; and source (brew --prefix asdf)/libexec/asdf.fish
-
-    if test -d (brew --prefix)"/share/fish/completions"
-        set -gx fish_complete_path $fish_complete_path (brew --prefix)/share/fish/completions
+    function fish_user_key_bindings
+        for mode in insert default visual
+            bind -M $mode \cf forward-char
+        end
     end
 
-    if test -d (brew --prefix)"/share/fish/vendor_completions.d"
-        set -gx fish_complete_path $fish_complete_path (brew --prefix)/share/fish/vendor_completions.d
+    # init zoxide
+    complete -e j
+    if command -sq zoxide
+        zoxide init --cmd j fish | source
+    else
+        echo 'zoxide: command not found, please install it from https://github.com/ajeetdsouza/zoxide'
     end
-end
 
-### MANAGED BY RANCHER DESKTOP START (DO NOT EDIT)
-set --export --prepend PATH "/Users/tomas/.rd/bin"
-### MANAGED BY RANCHER DESKTOP END (DO NOT EDIT)
+    atuin init fish | source
+
+    if type -q brew
+
+        [ -e (brew --prefix asdf)/libexec/asdf.fish ]; and source (brew --prefix asdf)/libexec/asdf.fish
+
+        if test -d (brew --prefix)"/share/fish/completions"
+            set -gx fish_complete_path $fish_complete_path (brew --prefix)/share/fish/completions
+        end
+
+        if test -d (brew --prefix)"/share/fish/vendor_completions.d"
+            set -gx fish_complete_path $fish_complete_path (brew --prefix)/share/fish/vendor_completions.d
+        end
+    end
+
+    ### MANAGED BY RANCHER DESKTOP START (DO NOT EDIT)
+    set --export --prepend PATH "/Users/tomas/.rd/bin"
+    ### MANAGED BY RANCHER DESKTOP END (DO NOT EDIT)
+end
