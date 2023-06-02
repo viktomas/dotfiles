e828210eaa9e5c3a01f4c46b3656179ef715f207
Tomas Vik <me@viktomas.com>
2023-06-02 11:45:26 +0200

    chore: automatic formatting for lua files

--- nvim/.config/nvim/lua/custom/plugins/fuzzy-search.lua
+++ nvim/.config/nvim/lua/custom/plugins/fuzzy-search.lua
@@ -1,64 +1,61 @@
 return {
-  'nvim-telescope/telescope.nvim',
-  dependencies = {'nvim-lua/plenary.nvim'},
-  config = function()
+	"nvim-telescope/telescope.nvim",
+	dependencies = { "nvim-lua/plenary.nvim" },
+	config = function()
+		local telescopeConfig = require("telescope.config")
+		local telescopeActions = require("telescope.actions")
 
-    local telescopeConfig = require("telescope.config")
-    local telescopeActions = require("telescope.actions")
+		-- grep through hidden files
+		-- https://github.com/nvim-telescope/telescope.nvim/wiki/Configuration-Recipes#file-and-text-search-in-hidden-files-and-directories
+		-- Clone the default Telescope configuration
+		local vimgrep_arguments = { unpack(telescopeConfig.values.vimgrep_arguments) }
 
-    -- grep through hidden files
-    -- https://github.com/nvim-telescope/telescope.nvim/wiki/Configuration-Recipes#file-and-text-search-in-hidden-files-and-directories
-    -- Clone the default Telescope configuration
-    local vimgrep_arguments = { unpack(telescopeConfig.values.vimgrep_arguments) }
+		-- I want to search in hidden/dot files.
+		table.insert(vimgrep_arguments, "--hidden")
 
-    -- I want to search in hidden/dot files.
-    table.insert(vimgrep_arguments, "--hidden")
+		require("telescope").setup({
+			defaults = {
+				-- `hidden = true` is not supported in text grep commands.
+				vimgrep_arguments = vimgrep_arguments,
+				mappings = {
+					i = {
+						["<C-u>"] = false, -- clear the prompt with CTRL-U
+						["<esc>"] = telescopeActions.close, --close the picker on ESC
+						-- ctrl+J and ctrl+K to scroll through the selection to match my FZF muscle memory
+						-- ["<C-j>"] = telescopeActions.move_selection_next,
+						-- ["<C-k>"] = telescopeActions.move_selection_previous,
+					},
+				},
+			},
+		})
 
-    require('telescope').setup({
-      defaults = {
-        -- `hidden = true` is not supported in text grep commands.
-        vimgrep_arguments = vimgrep_arguments,
-        mappings = {
-          i = {
-            ["<C-u>"] = false, -- clear the prompt with CTRL-U
-            ["<esc>"] = telescopeActions.close, --close the picker on ESC
-            -- ctrl+J and ctrl+K to scroll through the selection to match my FZF muscle memory
-            -- ["<C-j>"] = telescopeActions.move_selection_next,
-            -- ["<C-k>"] = telescopeActions.move_selection_previous,
-          }
-        },
-      },
-    })
+		local builtin = require("telescope.builtin")
 
-    local builtin = require('telescope.builtin')
+		local find_files = function()
+			builtin.find_files({ hidden = true })
+		end
 
-    local find_files = function()
-      builtin.find_files({ hidden = true })
-    end
+		local function getVisualSelection()
+			vim.cmd('noau normal! "vy"')
+			local text = vim.fn.getreg("v")
+			vim.fn.setreg("v", {})
 
-    local function getVisualSelection()
-      vim.cmd('noau normal! "vy"')
-      local text = vim.fn.getreg('v')
-      vim.fn.setreg('v', {})
+			text = string.gsub(text, "\n", "")
+			if #text > 0 then
+				return text
+			else
+				return ""
+			end
+		end
 
-      text = string.gsub(text, "\n", "")
-      if #text > 0 then
-        return text
-      else
-        return ''
-      end
-    end
-
-    vim.keymap.set('n', '<leader>f', find_files, {desc = 'find files'})
-    vim.keymap.set('n', "<leader>'", builtin.resume, {desc = 'resume last picker'})
-    vim.keymap.set('n', '<leader>b', builtin.buffers, {desc = 'select buffer'})
-    vim.keymap.set('n', '//', builtin.live_grep, {})
-    vim.keymap.set('v', '//', function ()
-      local text = getVisualSelection()
-      builtin.live_grep({default_text = text})
-    end)
-    vim.keymap.set('n', '<F1>', builtin.help_tags, {desc = 'search help tags'})
-
-  end
+		vim.keymap.set("n", "<leader>f", find_files, { desc = "find files" })
+		vim.keymap.set("n", "<leader>;", builtin.resume, { desc = "resume last picker" })
+		vim.keymap.set("n", "<leader>b", builtin.buffers, { desc = "select buffer" })
+		vim.keymap.set("n", "//", builtin.live_grep, { desc = "live grep" })
+		vim.keymap.set("v", "//", function()
+			local text = getVisualSelection()
+			builtin.live_grep({ default_text = text })
+		end)
+		vim.keymap.set("n", "<F1>", builtin.help_tags, { desc = "search help tags" })
+	end,
 }
-
--- nvim/.config/nvim/lua/custom/plugins/test.lua
+++ nvim/.config/nvim/lua/custom/plugins/test.lua
@@ -1,14 +1,14 @@
 return {
-  'vim-test/vim-test',
-  config = function()
-    local opts = { silent = true }
+	"vim-test/vim-test",
+	config = function()
+		local opts = { silent = true }
 
-    vim.g["test#strategy"] = "neovim"
-    vim.keymap.set("n", "<leader>tt", ":TestNearest<CR>", opts)
-    vim.keymap.set("n", "<leader>tf", ":TestFile<CR>", opts)
-    vim.keymap.set("n", "<leader>ta", ":TestSuite<CR>", opts)
-    vim.keymap.set("n", "<leader>tl", ":TestLast<CR>", opts)
-    vim.keymap.set("n", "<leader>tg", ":TestVisit<CR>", opts)
-    vim.keymap.set("t", "<C-o>", [[<C-\><C-n>]], opts)
-  end
+		vim.g["test#strategy"] = "neovim"
+		vim.keymap.set("n", "<leader>tt", ":TestNearest<CR>", opts)
+		vim.keymap.set("n", "<leader>tf", ":TestFile<CR>", opts)
+		vim.keymap.set("n", "<leader>ta", ":TestSuite<CR>", opts)
+		vim.keymap.set("n", "<leader>tl", ":TestLast<CR>", opts)
+		vim.keymap.set("n", "<leader>tg", ":TestVisit<CR>", opts)
+		vim.keymap.set("t", "<C-o>", [[<C-\><C-n>]], opts)
+	end,
 }
