8d6f7d0e95604855c66cf41a3d1eba49b29e2d6c
Tomas Vik <me@viktomas.com>
2023-07-03 08:39:54 +0200

    vim(oil): override default keybindings so I can use my C-h and C-l for switching splits

--- nvim/.config/nvim/lua/custom/plugins/oil.lua
+++ nvim/.config/nvim/lua/custom/plugins/oil.lua
@@ -4,6 +4,30 @@ return {
 		view_options = {
 			show_hidden = true,
 		},
+		skip_confirm_for_simple_edits = true,
+		-- Keymaps in oil buffer. Can be any value that `vim.keymap.set` accepts OR a table of keymap
+		-- options with a `callback` (e.g. { callback = function() ... end, desc = "", nowait = true })
+		-- Additionally, if it is a string that matches "actions.<name>",
+		-- it will use the mapping at require("oil.actions").<name>
+		-- Set to `false` to remove a keymap
+		-- See :help oil-actions for a list of all available actions
+		keymaps = {
+			["g?"] = "actions.show_help",
+			["<CR>"] = "actions.select",
+			["<C-v>"] = "actions.select_vsplit",
+			["<C-s>"] = "actions.select_split",
+			["<C-t>"] = "actions.select_tab",
+			["<C-p>"] = "actions.preview",
+			["<C-c>"] = "actions.close",
+			["<C-r>"] = "actions.refresh",
+			["-"] = "actions.parent",
+			["_"] = "actions.open_cwd",
+			["`"] = "actions.cd",
+			["~"] = "actions.tcd",
+			["g."] = "actions.toggle_hidden",
+		},
+		-- Set to false to disable all of the above keymaps
+		use_default_keymaps = false,
 	},
 	-- Optional dependencies
 	dependencies = { "nvim-tree/nvim-web-devicons" },
