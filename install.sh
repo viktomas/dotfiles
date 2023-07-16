1260cdcfeeb8bf00577e41ab8b6e13d8f63be960
Tomas Vik <me@viktomas.com>
2023-07-16 11:00:39 +0200

    espanso: add config

--- install.sh
+++ install.sh
@@ -12,6 +12,7 @@ ln -s $LOCATION/bin/ ~/
 ln -s $LOCATION/.global-gitignore ~/.gitignore
 ln -s $LOCATION/.goduignore ~/.goduignore
 ln -s $LOCATION/.notable.json ~/.notable.json
+[ "$(uname 2> /dev/null)" == "Darwin" ] && ln -s "$LOCATION/espanso/base.yml" "$HOME/Library/Application Support/espanso/match/base.yml"
 [ "$(uname 2> /dev/null)" == "Darwin" ] && ln -s "$LOCATION/vscode/settings.json" "$HOME/Library/Application Support/Code/User/settings.json"
 [ "$(uname 2> /dev/null)" == "Darwin" ] && ln -s "$LOCATION/vscode/settings.json" "$HOME/Library/Application Support/Code - Insiders/User/settings.json"
 [ "$(uname 2> /dev/null)" == "Linux" ] && ln -s "$LOCATION/vscode/settings.json" "$HOME/.config/Code - OSS/User/settings.json"
@@ -31,4 +32,3 @@ chmod 700 ~/.vim ~/.vim/backup/ ~/.vim/undo/ ~/.vim/swap/ # sensitive informatio
 
 
 [[ $SHELL != *"fish" ]] && chsh -s $(which fish)
-
