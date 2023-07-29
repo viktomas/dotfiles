7017e288a93099ad8e65491c021bf34763c9b3aa
Tomas Vik <me@viktomas.com>
2023-07-29 10:31:32 +0200

    nvim: add tr go snippet

--- nvim/.config/nvim/lua/custom/plugins/snippets.lua
+++ nvim/.config/nvim/lua/custom/plugins/snippets.lua
@@ -7,20 +7,32 @@ return {
 		history = true,
 		delete_check_events = "TextChanged",
 	},
-    -- stylua: ignore
-    keys = {
-      {
-        "<tab>",
-        function()
-          return require("luasnip").jumpable(1) and "<Plug>luasnip-jump-next" or "<tab>"
-        end,
-        expr = true, silent = true, mode = "i",
-      },
-      { "<tab>", function() require("luasnip").jump(1) end, mode = "s" },
-      { "<s-tab>", function() require("luasnip").jump(-1) end, mode = { "i", "s" } },
-      -- <NL> is C-j in lua
-      { "<NL>", function () require("luasnip").expand() end, mode ="i"},
-    },
+	keys = {
+		{
+			"<tab>",
+			function()
+				local luasnip = require("luasnip")
+				if luasnip.expand_or_jumpable() then
+					print("expand or jumpable")
+					luasnip.expand_or_jump()
+				else
+					print("not expand_or_jumpable")
+					-- feed the normal tab
+					vim.api.nvim_feedkeys(vim.api.nvim_replace_termcodes("<Tab>", true, false, true), "n", false)
+				end
+			end,
+			mode = { "i", "s" },
+		},
+		{
+			"<s-tab>",
+			function()
+				require("luasnip").jump(-1)
+			end,
+			mode = { "i", "s" },
+		},
+		-- -- <NL> is C-j in lua
+		-- { "<NL>", function () require("luasnip").expand() end, mode ="i"},
+	},
 	config = function()
 		require("luasnip.loaders.from_vscode").load({ paths = "./snippets" })
 	end,
