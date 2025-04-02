d458c60cb7a77f75bb3705bd8390d07485acf285
Tomas Vik <me@viktomas.com>
2025-04-02 09:36:46 +0200

    fish: fzf_history_search - ctrl+r to switch mode

--- fish/.config/fish/functions/fzf_history_search.fish
+++ fish/.config/fish/functions/fzf_history_search.fish
@@ -2,12 +2,14 @@ function fzf_history_search
     set -l initial_query (commandline -b)
 
     history | fzf \
-      --query "$initial_query" \
-      --no-sort \
-      --tiebreak=index \
-      --scheme=history \
-      | read -l command
-    
+        --query "$initial_query" \
+        --no-sort \
+        --bind=ctrl-r:toggle-sort \
+        --highlight-line \
+        --tiebreak=index \
+        --scheme=history \
+        | read -l command
+
     if test -n "$command"
         commandline -f repaint
         commandline -r $command
