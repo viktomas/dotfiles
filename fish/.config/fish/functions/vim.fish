b0a5fddba6c1598a96250fff0d181e79553ce981
Tomas Vik <me@viktomas.com>
2025-04-02 10:58:54 +0200

    fish: use suspended nvim instead of starting new session

--- fish/.config/fish/functions/vim.fish
+++ fish/.config/fish/functions/vim.fish
@@ -1,4 +1,13 @@
 function vim --wraps=nvim --description 'alias vim nvim'
-  nvim $argv
-        
+    # this will get the group id of last suspended job
+    set -f existing_nvim_group_id (jobs | grep 'nvim' | awk '{print $2}' | head --lines 1)
+    if test -n "$existing_nvim_group_id"
+        if test -n "$argv"
+            echo 'There is a supspended nvim instance, I will start that and ignore your params. Press [Enter] to continue.'
+            read # wait for user confirmation
+        end
+        fg $existing_nvim_group_id
+    else
+        nvim $argv
+    end
 end
